b1f8e0b3dfeba7fd7bada86a2c8605fcaa6e164f
ram25493 <ram.official25@gmail.com>
2019-03-29 12:57:19 +0530

    even-odd

--- math-homework.js
+++ math-homework.js
@@ -1,12 +1,10 @@
-var a=30
-var b=20
-console.log("The addition is " + (a+b))
-console.log("The subtraction is " + (a-b))
-console.log("The multiplication is " + (a*b))
-console.log("The division is " + (a/b))
+var num = 25;
 
-
-
+if (num % 2 == 0) {
+    console.log("Number is even");
+} else {
+    console.log("Number is odd");
+}
 
 
 
