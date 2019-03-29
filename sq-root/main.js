37e310283d135a247eade5b9bd6e269d70272033
ram25493 <ram.official25@gmail.com>
2019-03-30 00:10:49 +0530

    homework2

---
+++ sq-root/main.js
@@ -0,0 +1,12 @@
+function square(){
+    var no;
+    no=Number(document.getElementById("nsquare").value);
+
+    alert("Square is" + no*no);
+    }
+    function squareroot(){
+        var no1;
+        no1=Number(document.getElementById("nsquareroot").value);
+        var value =Math.sqrt(no1);
+        alert("Square root is" + value);
+        }
